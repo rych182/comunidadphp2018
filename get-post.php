3b04f2a264dad192c43428fa58e4c20337dad1a1
Ricardo Garrido <ricgarridoc@hotmail.com>
2018-12-12 12:00:08 -0600

    agregue comentarios de get y post

--- get-post.php
+++ get-post.php
@@ -12,7 +12,7 @@
 	<div>
 		Esto es una página para formulario de html con PHP
 	</div>
-	<form action="recibir.php?edad=23" method="POST">
+	<form action="recibir.php" method="POST">
 		Nombre: <input type="text" name="nombreformulario" id="idnombreformulario">
 		<br>
 		<input type="submit" value="Enviar">
@@ -21,6 +21,9 @@
 	name es el nombre generico al componente que trabajas y puede repetirse en cualquier otro elemento.
 
 	id: el identificador ÚNICO que nos sirve para el DOM
+
+	El truquito para enviar datos por post a get en recibir.php
+	<form action="recibir.php?edad=23" method="POST">
 -->
 </body>
 </html>
--- recibir.php
+++ recibir.php
@@ -5,10 +5,16 @@ class miSistema
 	function __construct()
 	{
 		echo "Post: ".$_POST["nombreformulario"]."<br>";
-		echo "Get: ".$_GET["edad"]."<br>";
+		echo "Get: ".$_GET["nombreformulario"]."<br>";
 
 		echo "Inicia archivo recibir";
 	}
 }
 $miObjeto = new miSistema();
+
+
+
+//El truquito para mandar datos en la url con el metodo POST
+//echo "Get: ".$_GET["edad"]."<br>";
+
 ?>
